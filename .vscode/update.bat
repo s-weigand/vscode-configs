c88f71e160421acd4504e0604213fdae03d40d98
s-weigand <s.weigand.phy@gmail.com>
2019-12-16 12:30:22 +0100

    removed the base .vscode and exchanged it for a symlink to the python config

--- .vscode/update.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git clone https://github.com/s-weigand/vscode-configs.git
-xcopy /Y %~dp0vscode-configs\python\.vscode\*.*
-rmdir  /Q /S vscode-configs
--- .vscode/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/s-weigand/vscode-configs.git
-cp vscode-configs/python/.vscode/* ./
-rm -rf vscode-configs
